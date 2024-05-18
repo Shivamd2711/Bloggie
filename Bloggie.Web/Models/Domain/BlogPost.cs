343c6ba6a5e8381f699a0050eadadead9b2acfde
Shivamd2711 <shivamdalvi10@gmail.com>
2024-05-18 10:50:54 +0530

    Add project files.

---
+++ Bloggie.Web/Models/Domain/BlogPost.cs
@@ -0,0 +1,18 @@
+﻿namespace Bloggie.Web.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Heading { get; set; }
+        public string PageTitle { get; set; }
+        public string  Content { get; set; }
+        public string ShortDesciption { get; set; }
+        public string FeaturedImageUrl { get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime PublishDate { get; set; }
+        public string Author { get; set; }
+        public bool Visible { get; set; }
+
+        public ICollection<Tag> Tags { get;}
+    }
+}
